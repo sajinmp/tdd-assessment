0208dce85cb189623dee5f077ad597566dd35637
sajinmp <sajinprasadkm@gmail.com>
2025-07-24 14:25:42 +0530

    raise error if negative numbers present in input

--- string_calculator.rb
+++ string_calculator.rb
@@ -8,10 +8,24 @@ class StringCalculator
   def add
     return 0 if input.empty?
     
-    input.gsub("\n", delimiter).split(delimiter).map(&:to_i).sum
+    raise StandardError.new(error_message) if negative_numbers.any?
+
+    numbers.sum
   end
 
   private
+
+  def error_message
+    "negative numbers not allowed #{negative_numbers.join(',')}"
+  end
+
+  def numbers
+    input.gsub("\n", delimiter).split(delimiter).map(&:to_i)
+  end
+
+  def negative_numbers
+    numbers.select(&:negative?)
+  end
 
   def delimiter
     input[0..1] == "//" ? input[2] : ","
